db046ba32493221244f9b7810ed52097f9215d58
Abhishek-Krishna-A-M <abhishekkrishna2k6@gmail.com>
2025-10-07 20:37:25 +0530

    Restarting the project, codebase setup completed

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerUser, authUser } = require('../controllers/authController');
-const loginLimiter = require('../middleware/loginLimiter')
-const router = express.Router();
-
-router.route('/register').post(registerUser);
-router.post('/login', loginLimiter, authUser);
-
-module.exports = router;
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { allUsers } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.route('/').get(protect, allUsers);
-
-module.exports = router;
