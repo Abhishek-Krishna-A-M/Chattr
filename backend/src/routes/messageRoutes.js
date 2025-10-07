f7e5cfafb3881b7dba8686e6be7d85d244c80764
Abhishek-Krishna-A-M <abhishekkrishna2k6@gmail.com>
2025-10-07 22:23:47 +0530

    Stack change

--- backend/src/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const router = express.Router()
-
-router.get('/send', (req, res) => {
-    res.send("Send message route")
-})
-
-export default router
---
+++ src/app/auth/callback/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div>Auth callback page</div>
+  )
+}
+
+export default page
